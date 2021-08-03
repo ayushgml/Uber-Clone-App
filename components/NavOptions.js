d898e6fcc9de8bcc1b023ac50d140a400564d85a
ayushgml <72748253+ayushgml@users.noreply.github.com>
2021-08-03 17:18:49 +0530

    Add files via upload

---
+++ components/NavOptions.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const NavOptions = () => {
+    return (
+        <View>
+            <Text>This is Navigation</Text>
+        </View>
+    )
+}
+
+export default NavOptions
+
+const styles = StyleSheet.create({})
